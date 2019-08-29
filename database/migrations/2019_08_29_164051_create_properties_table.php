32a4ca9d9b9dcee9a8cf873a53c25a12a475d9f1
Makara Chhin <cmpsnl310320@gmail.com>
2019-08-30 00:14:36 +0700

    Create properties table model using migration

---
+++ database/migrations/2019_08_29_164051_create_properties_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code')->unique();
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('restrict');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('excerpt');
+            $table->string('image');
+            $table->tinyInteger('objection')->default(0);
+            $table->integer('bedroom');
+            $table->integer('bathroom');
+            $table->string('size');
+            $table->text('details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
